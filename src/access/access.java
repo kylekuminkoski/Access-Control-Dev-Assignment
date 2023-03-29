da504d90f99901c5e70db9419659f68a938dcebb
kylekuminkoski <78025486+kylekuminkoski@users.noreply.github.com>
2023-03-28 23:58:02 -0400

    Add files via upload

---
+++ src/access/access.java
@@ -0,0 +1,21 @@
+package access;
+
+public class access {
+
+	public static void main(String[] args) {
+		
+		//initialize file
+		
+		//check if first line of file is 'friendadd', if yes move on call function and get next line, if no throw error
+		
+		//while instruction is not 'end'
+		
+		
+		
+		
+		//get next instruction
+		
+
+	}
+
+}
