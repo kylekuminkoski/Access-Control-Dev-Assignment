42c8f5f89b6e5c769835ddcd8a68f7e7464e72d0
Kyle Kuminkoski <kyle.kumin@gmail.com>
2023-03-29 22:34:28 -0400

    Log creation functionality added
    Added method for creating log files. Method name: createLog(File name)
    Added four calls to this method in main to create audit, friends, lists,
    and pictures log.

--- src/access/access.java
+++ src/access/access.java
@@ -1,34 +1,53 @@
 package access;
 
-import java.io.Console;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class access {
-
+	static File instructions;
+	static PrintStream auditOut;
+	static PrintStream consoleOut;
+	static PrintStream friendsOut;
+	static PrintStream listsOut;
+	static PrintStream picturesOut;
+	
 	public static void main(String[] args) {
+		//Console Print Stream
+		consoleOut = System.out;
+		
 		//Create nil list
 		List nil = new List();
-
+		
+		//Create Audit Logs
 		File audit = new File("audit.txt");
-		try {
-			audit.delete();
-			audit.createNewFile();
-			PrintStream out = new PrintStream(new FileOutputStream(audit));
-			System.setOut(out);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		auditOut = createLog(audit);
+		
+		//Create Friends Log
+		File friends = new File("friends.txt");
+		friendsOut = createLog(friends);
+		
+		//Create Lists Log
+		File lists = new File ("lists.txt");
+		listsOut = createLog(friends);
+		
+		//Create Pictures Log
+		File pictures = new File ("pictures.txt");
+		listsOut = createLog(pictures);		
 
-		//initialize file
+		//Read in instructions from arguments. If argument is not given, throws error.
+		if(args.length != 0) {
 		String inputFile = args[0];
-		File instructions = new File(inputFile);
+		instructions = new File(inputFile);
+		}else {
+			instructions = new File("myTestCase.txt");
+		}
+		
+		
 		
 		//check if first line of file is 'friendadd', if yes move on call function and get next line, if no output   error
 		Scanner reader = null;
@@ -41,6 +60,16 @@ public class access {
 		
 		//while instruction is not 'end'
 		
+		while(reader.hasNextLine()) {
+			String line = reader.nextLine();
+			StringTokenizer tokens = new StringTokenizer(line);
+			while(tokens.hasMoreTokens()) {
+				String token = tokens.nextToken();
+				consoleOut.println(token);
+				auditOut.println(token);
+			}
+			
+		}
 		
 		
 		
@@ -48,6 +77,24 @@ public class access {
 		
 
 		reader.close();
+	}
+	
+	
+	//Helper method designed to simplify the file creation, overwrite, and output for each log.
+	//Method will try
+	public static PrintStream createLog(File name) {
+		PrintStream print;
+		try {
+			name.delete();
+			name.createNewFile();
+			print = new PrintStream(new FileOutputStream(name));
+			return print;
+		} catch (IOException e) {
+			consoleOut.println("Failed to create \"" + name.getName() + "\" file. Exiting...");
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 	
 	public void friendadd(String friendname) {
